5528c3a168d595f5dd4450e105bce382ad6eb16b
VVladyslav <VVVasylyshyn@gmail.com>
2022-10-13 23:26:24 +0300

    no scroll
    додав no scroll  в common.css  &  body
    
    атрибути в js та  mмодалку

--- src/js/modal-product-ingredients.js
+++ src/js/modal-product-ingredients.js
@@ -1,14 +1,20 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-product-ingredients-open]'),
+    openModalBtn1: document.querySelector('[data-modal-product-ingredients-open1]'),
+    openModalBtn2: document.querySelector('[data-modal-product-ingredients-open2]'),
+    openModalBtn3: document.querySelector('[data-modal-product-ingredients-open3]'),
     closeModalBtn: document.querySelector('[data-modal-product-ingredients-close]'),
     modal: document.querySelector('[data-modal-product-ingredients]'),
+    bodyNoScroll: document.querySelector("[data-no-scroll]")
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.openModalBtn1.addEventListener('click', toggleModal);
+  refs.openModalBtn2.addEventListener('click', toggleModal);
+  refs.openModalBtn3.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-
+  
   function toggleModal() {
     refs.modal.classList.toggle('is-hidden');
+    refs.bodyNoScroll.classList.toggle('no-scroll');
   }
 })();
